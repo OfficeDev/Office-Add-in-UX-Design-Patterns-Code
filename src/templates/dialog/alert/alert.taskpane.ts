8f08adcf4a90d585257ac3e7af314dc97c80ce78
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-02-14 11:48:06 -0800

    updating dialog to use typescript

---
+++ src/templates/dialog/alert/alert.taskpane.ts
@@ -0,0 +1,13 @@
+﻿/* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE in the project root for license information. */
+
+(() => {
+    Office.initialize = () => {
+        $(document).ready(() => {
+            $('#action-button').click(handleButton);
+        });
+    };
+
+    function handleButton() {
+        app.openDialog("https://localhost:3000/templates/dialog/alert/alert.html", 10, 30);
+    }
+})();
