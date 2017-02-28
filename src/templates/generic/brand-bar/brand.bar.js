e2459911f870289fed6f2685abafdc1f39e40799
Chris Lockwood <chris.lockwood11@gmail.com>
2017-02-27 16:44:36 -0800

    More brand bar changes

--- src/templates/generic/brand-bar/brand.bar.js
+++ src/templates/generic/brand-bar/brand.bar.js
@@ -4,8 +4,8 @@
             initializeSettingsMenu();
         };
         function initializeSettingsMenu() {
-            var button = $('#settingsMenu');
-            $('.ms-ContextualMenu').map(function (i, menu) { return new fabric['ContextualMenu'](menu, button); });
+            var $button = $('#settingsMenu');
+            $('.ms-ContextualMenu').map(function (i, menu) { new fabric['ContextualMenu'](menu, $button[0]); });
         }
     });
 })(jQuery);
