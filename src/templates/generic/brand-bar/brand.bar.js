46ee16665edaab1b869e49ff91c46f428988505d
Chris Lockwood <chris.lockwood11@gmail.com>
2017-02-27 16:19:15 -0800

    More brand bar updates

--- src/templates/generic/brand-bar/brand.bar.js
+++ src/templates/generic/brand-bar/brand.bar.js
@@ -1,13 +1,11 @@
 (function ($) {
     $(document).ready(function () {
-        function initializeContextualMenu() {
-            var ContextualMenuElements = document.querySelectorAll(".dp-brand-bar--right");
-            for (var i = 0; i < ContextualMenuElements.length; i++) {
-                var ButtonElement = ContextualMenuElements[i].querySelector(".dp-brand-bar--right__button");
-                var ContextualMenuElement = ContextualMenuElements[i].querySelector(".ms-ContextualMenu");
-                new fabric['ContextualMenu'](ContextualMenuElement, ButtonElement);
-            }
+        Office.initialize = function () {
+            initializeSettingsMenu();
+        };
+        function initializeSettingsMenu() {
+            var button = $('#settingsMenu');
+            $('.ms-ContextualMenu').map(function (i, menu) { return new fabric['ContextualMenu'](menu, button); });
         }
-        initializeContextualMenu();
     });
 })(jQuery);
--- src/templates/generic/brand-bar/brand.bar.ts
+++ src/templates/generic/brand-bar/brand.bar.ts
@@ -2,16 +2,14 @@ declare var fabric: any;
 
 (($) => {
     $(document).ready(() => {
-        function initializeContextualMenu() {
-            var ContextualMenuElements = document.querySelectorAll(".dp-brand-bar--right");
-            for (var i = 0; i < ContextualMenuElements.length; i++) {
-                var ButtonElement = ContextualMenuElements[i].querySelector(".dp-brand-bar--right__button");
-                var ContextualMenuElement = ContextualMenuElements[i].querySelector(".ms-ContextualMenu");
-            
-                new fabric['ContextualMenu'](ContextualMenuElement, ButtonElement);
-            }
+        Office.initialize = () => { 
+            initializeSettingsMenu(); 
+        };
+
+        function initializeSettingsMenu() {
+            let $button = $('#settingsMenu');            
+            $('.ms-ContextualMenu').map((i, menu)=>{ new fabric['ContextualMenu'](menu, $button[0])});
         }
-        initializeContextualMenu();
     });
 })(jQuery);
 
