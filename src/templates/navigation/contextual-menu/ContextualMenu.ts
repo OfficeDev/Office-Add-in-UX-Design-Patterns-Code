4ac9e99e5a73f512044eff144909a3d3eea98b9b
Chris Lockwood <chris.lockwood11@gmail.com>
2017-07-05 16:08:38 -0700

    Updated contextual menu code

---
+++ src/templates/navigation/contextual-menu/ContextualMenu.ts
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+// See LICENSE in the project root for license information.
+
+declare var fabric: any;
+
+(($) => {
+    $(document).ready(() => {
+        Office.initialize = () => { 
+            initializeSettingsMenu(); 
+        };
+
+        function initializeSettingsMenu() {
+            let $button = $('#mainMenu');            
+            $('.ms-ContextualMenu').map((i, menu)=>{ new fabric['ContextualMenu'](menu, $button[0])});
+        }
+
+        //$('.ms-ContextualMenu-link:has(.ms-Icon)').css('lineHeight', '0')
+    });
+})(jQuery);
